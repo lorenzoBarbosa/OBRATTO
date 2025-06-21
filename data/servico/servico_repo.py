0b1300a195bff305401b5da8ae285d10d72bd85f
gabriellyvidal <gabriellyvidal.ifes@gmail.com>
2025-06-21 09:51:51 -0300

    Padronização da tabela servico

--- data/servico/servico_repo.py
+++ data/servico/servico_repo.py
@@ -1,21 +1,21 @@
 from typing import List, Optional
 from data.servico.servico_model import Servico
-from data.servico.servico_sql import CRIAR_TABELA, INSERIR, OBTER_TODOS, OBTER_POR_ID, UPDATE, DELETE
+from data.servico.servico_sql import CRIAR_TABELA_SERVICO, INSERIR_SERVICO, OBTER_SERVICO, OBTER_SERVICO_POR_ID, ATUALIZAR_SERVICO, DELETAR_SERVICO
 from utils.db import open_connection
 
 
-def criar_tabela() -> bool:
+def criar_tabela_servico() -> bool:
     with open_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(CRIAR_TABELA)
+        cursor.execute(CRIAR_TABELA_SERVICO)
         conn.commit()
         return True
 
 
-def inserir(servico: Servico) -> Optional[int]:
+def inserir_servico(servico: Servico) -> Optional[int]:
     with open_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(INSERIR, (
+        cursor.execute(INSERIR_SERVICO, (
             servico.id_prestador,
             servico.titulo,
             servico.descricao,
@@ -26,10 +26,10 @@ def inserir(servico: Servico) -> Optional[int]:
         return cursor.lastrowid
 
 
-def obter_todos() -> List[Servico]:
+def obter_servico() -> List[Servico]:
     with open_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(OBTER_TODOS)
+        cursor.execute(OBTER_SERVICO)
         rows = cursor.fetchall()
         servicos = []
         for row in rows:
@@ -45,10 +45,10 @@ def obter_todos() -> List[Servico]:
         return servicos
 
 
-def obter_por_id(id_servico: int) -> Optional[Servico]:
+def obter_servico_por_id(id_servico: int) -> Optional[Servico]:
     with open_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(OBTER_POR_ID, (id_servico,))
+        cursor.execute(OBTER_SERVICO_POR_ID, (id_servico,))
         row = cursor.fetchone()
         if row:
             return Servico(
@@ -63,13 +63,13 @@ def obter_por_id(id_servico: int) -> Optional[Servico]:
         return None
 
 
-def atualizar(servico:Servico) -> bool:
+def atualizar_servico(servico:Servico) -> bool:
     """
     Atualiza dados da tabela servico.
     """
     with open_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(UPDATE, (
+        cursor.execute(ATUALIZAR_SERVICO, (
             servico.id_prestador,
             servico.titulo,
             servico.descricao,
@@ -81,9 +81,9 @@ def atualizar(servico:Servico) -> bool:
         return cursor.rowcount > 0
 
 
-def deletar(id_servico: int) -> bool:
+def deletar_servico(id_servico: int) -> bool:
     with open_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(DELETE, (id_servico,))
+        cursor.execute(DELETAR_SERVICO, (id_servico,))
         conn.commit()
         return cursor.rowcount > 0
--- data/servico/servico_sql.py
+++ data/servico/servico_sql.py
@@ -1,21 +1,21 @@
-CRIAR_TABELA = """
+CRIAR_TABELA_SERVICO = """
 CREATE TABLE IF NOT EXISTS servico(
-id_servico INTEGER PRIMARY KEY AUTOINCREMENT,
-id_prestador INTEGER NOT NULL,
-titulo TEXT NOT NULL,
-descricao TEXT NOT NULL, 
-categoria TEXT NOT NULL,
-valor_base REAL NOT NULL,
-FOREIGN KEY (id_prestador) REFERENCES prestador(id)
+    id_servico INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_prestador INTEGER NOT NULL,
+    titulo TEXT NOT NULL,
+    descricao TEXT NOT NULL, 
+    categoria TEXT NOT NULL,
+    valor_base REAL NOT NULL,
+    FOREIGN KEY (id_prestador) REFERENCES prestador(id)
 );
 """
 
-INSERIR = """
+INSERIR_SERVICO = """
 INSERT INTO servico (id_prestador, titulo, descricao, categoria, valor_base)
 VALUES  (?, ?, ?, ?, ?);
 """
 
-OBTER_TODOS = """
+OBTER_SERVICO = """
 SELECT 
     s.id_servico,
     s.id_prestador,
@@ -30,7 +30,7 @@ JOIN usuario u ON p.id_usuario = u.id
 ORDER BY s.titulo
 """    
 
-OBTER_POR_ID = """
+OBTER_SERVICO_POR_ID = """
 SELECT 
    s.id_servico,
    s.id_prestador,
@@ -45,13 +45,17 @@ JOIN usuario u ON p.id_usuario = u.id
 WHERE s.id_servico = ?;
 """
 
-UPDATE = """
+ATUALIZAR_SERVICO = """
 UPDATE servico
-SET id_prestador = ?, titulo = ?, descricao = ?, categoria = ?, valor_base = ?
+SET id_prestador = ?,
+titulo = ?,
+descricao = ?,
+categoria = ?,
+valor_base = ?
 WHERE id_servico = ?;
 """
 
-DELETE= """
+DELETAR_SERVICO = """
 DELETE FROM servico
 WHERE id_servico = ?;
 """
