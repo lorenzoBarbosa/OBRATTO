8729d13aa86b25863aa0beebc24a34a1d5c46c4a
gabriellyvidal <gabriellyvidal.ifes@gmail.com>
2025-06-30 08:01:45 -0300

    test inscricao plano

--- tests/test_inscricao_plano.py
+++ tests/test_inscricao_plano.py
@@ -1,15 +1,14 @@
-
-
-
 import sys
 import os
 
-from data.fornecedor.fornecedor_model import Fornecedor
-from data.fornecedor.fornecedor_repo import criar_tabela_fornecedor
+from data.fornecedor.fornecedor_model import *
+from data.fornecedor.fornecedor_repo import *
 from data.inscricaoplano.inscricao_plano_repo import *
-from data.plano.plano_repo import criar_tabela_plano, inserir_plano
+from data.plano.plano_model import Plano
+from data.plano.plano_repo import *
 from data.prestador.prestador_model import Prestador
-from data.prestador.prestador_repo import criar_tabela_prestador
+from data.prestador.prestador_repo import *
+from data.usuario.usuario_repo import *
 
 class Test_InscricaoPlanoRepo:
     def test_criar_tabela_inscricao_plano(self, test_db):
@@ -44,47 +43,70 @@ class Test_InscricaoPlanoRepo:
         assert inscricao_plano_db.id_plano == id_plano
 
 
-    def test_obter_inscricao_plano(self, test_db):
-            # Arrange
-            criar_tabela_plano()
-            criar_tabela_prestador()
-            criar_tabela_fornecedor()
 
-            email_unico = "email_unico_para_teste@email.com"
-            usuario_teste = Usuario(0, "Usuario Unico", email_unico, "senha", "11122233344", "77777777777", datetime.now(), "Endereco Unico")
-            inserir_usuario(usuario_teste)
-            # Act
-            usuario_db = obter_usuario_por_email(email_unico)
-            # Assert
-            assert usuario_db is not None, "Deveria ter encontrado um usuário com o email especificado"
-            assert usuario_db.email == email_unico
 
     def test_obter_inscricao_plano_por_id(self, test_db):
-            criar_tabela_plano()
-            criar_tabela_prestador()
-            criar_tabela_fornecedor()
-     
-            inscricao_plano = InscricaoPlano(0, 0, 0)
-            id_plano = inserir_plano(id_plano)
-            prestador = Prestador (0, "", "", "", "", "", "", "", "", "", "", "")
-            id_prestador = id_prestador()
-            fornecedor = Fornecedor(0,"","","","","","","", "Obratto")
-            id_fornecedor = id_fornecedor(id_fornecedor, id_plano)
-            inscricaoplano = InscricaoPlano(0, 0, 0)
-            # Act fazer a ação que será testada
-            id_plano = inserir_plano(plano)
-            id_prestador = inserir_prestador(prestador)
-            id_fornecedor = inserir_fornecedor(fornecedor)
-            inscricao_plano_db = obter_inscricao_plano_por_id()
-    # Asserts verificar se o resultado é o esperado
-            assert id_plano is not None, "O anúncio não foi inserido com sucesso."
-            assert id_prestador is not None, "O anúncio não foi inserido com sucesso."
-            assert id_fornecedor is not None, "O anúncio não foi inserido com sucesso."
-
-
-    def test_atualizar_inscricao_plano(self, test_db):
-        #Arrange
+        criar_tabela_usuario()
+        criar_tabela_fornecedor()
         criar_tabela_plano()
         criar_tabela_prestador()
-        criar_tabela_fornecedor()
-        
+        criar_tabela_inscricao_plano()
+
+        fornecedor = Fornecedor(
+            0,
+            "Fornecedor Teste",
+            "Fornecedor LTDA",  
+            "12345678000199",  
+            "fornecedor@teste.com",
+            "27999999999",
+            "Rua dos Fornecedores",
+            "100",
+            "Centro",
+            "Obratto"
+    )
+        id_fornecedor = inserir_fornecedor(fornecedor)
+        assert id_fornecedor is not None, "Fornecedor não foi inserido com sucesso"
+
+        plano = Plano(
+            id_plano= 0,
+            nome="Plano Básico",
+            descricao="Acesso limitado",
+            valor_mensal="10",
+            limite_servico= "3",
+            tipo_plano="gratuito",
+            descricao="maquinario"
+    )
+        id_plano = inserir_plano(plano)
+        assert id_plano is not None, "Plano não foi inserido com sucesso"
+
+        prestador = Prestador(
+         id=0,
+         nome="Maria Prestadora",
+         cpf="11122233344",
+         email="maria@prestadora.com",
+         telefone="27988887777",
+         rua="Rua das Prestadoras",
+         numero="200",
+         bairro="Bairro B",
+         cidade="Cidade C",
+         estado="ES",
+         cep="29000-000",
+         senha="senha123"
+    )
+        id_prestador = inserir_prestador(prestador)
+        assert id_prestador is not None, "Prestador não foi inserido com sucesso"
+
+        inscricao = InscricaoPlano(
+            id=0,
+            id_prestador=id_prestador,
+            id_plano=id_plano
+    )
+        id_inscricao = inserir_inscricao_plano(inscricao)
+        assert id_inscricao is not None, "Inscrição de plano não foi inserida com sucesso"
+
+        inscricao_db = obter_inscricao_plano_por_id(id_inscricao)
+
+        assert inscricao_db is not None, "Inscrição de plano não foi encontrada"
+        assert inscricao_db.id == id_inscricao
+        assert inscricao_db.id_plano == id_plano
+        assert inscricao_db.id_prestador == id_prestador
