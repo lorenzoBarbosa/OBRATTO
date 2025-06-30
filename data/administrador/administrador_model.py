6549639857e208e18efb6332df451dc45553d594
Maria Eduarda <mariaeduardmozer.ifes@gmail.com>
2025-06-29 21:49:19 -0300

    testes - fornecedor (2 prontos)

--- data/administrador/administrador_model.py
+++ data/administrador/administrador_model.py
@@ -2,5 +2,6 @@ from dataclasses import dataclass
 from data.usuario.usuario_model import Usuario
 
 @dataclass
-class Administrador (Usuario):
+class Administrador:
+    id: int
     id_usuario: int 
