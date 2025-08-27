4e3c700dd4d981471e1725f9f7e7395de065faa8
Gabrielly Vidal <gabriellyvidal.ifes@gmail.com>
2025-08-27 16:21:09 -0300

    padronizacao do esquema de rotas

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from contextlib import asynccontextmanager
 from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
+from routes.publico import publico_routes
+from routes.fornecedor import fornecedor_produtos
+from routes.fornecedor import fornecedor_planos
+from routes.fornecedor import fornecedor_perfil
+from routes.fornecedor import fornecedor_promocoes
+from routes.fornecedor import fornecedor_solicitacoes
+from routes.administrador import administrador_anuncios
+from routes.administrador import administrador_usuarios
+from routes.prestador import prestador
+from routes.cliente import cliente
+
+
+
 app = FastAPI(
     title="Obratto",
     description="Plataforma para gerenciamento de produtos de fornecedores e serviços de prestadores.",
@@ -14,51 +27,24 @@ app.add_middleware(SessionMiddleware, secret_key="sua_chave_secreta")
 
 
 # PÚBLICO
-
-from routes.publico.publico_routes import router as publico_router # corrigido para refletir a estrutura correta
-
-
-app.include_router(publico_router)
+app.include_router(publico_routes.router)
 
 # FORNECEDOR
-
-from routes.fornecedor.fornecedor_produtos import router as fornecedor_produtos_router
-from routes.fornecedor.fornecedor_planos import router as fornecedor_planos_router
-from routes.fornecedor import fornecedor_perfil
-from routes.fornecedor import fornecedor_promocoes
-from routes.fornecedor import fornecedor_solicitacoes
-
-
 app.include_router(fornecedor_promocoes.router)
 app.include_router(fornecedor_perfil.router)
 app.include_router(fornecedor_solicitacoes.router)
-app.include_router(fornecedor_produtos_router)
-app.include_router(fornecedor_planos_router)
+app.include_router(fornecedor_produtos.router)
+app.include_router(fornecedor_planos.router)
 
 # ADMINISTRADOR
-
-from routes.administrador import administrador_anuncios
-from routes.administrador import administrador_usuarios
-
-
 app.include_router(administrador_usuarios.router)
 app.include_router(administrador_anuncios.router)
 
 # PRESTADOR
-
-from routes.prestador.prestador_router import router as prestador_router
-
-
-app.include_router(prestador_router, prefix="/prestador")
-
-
+app.include_router(prestador.router, prefix="/prestador")
 
 # CLIENTE
-
-from routes.cliente.cliente_router import router as cliente_router
-
-
-app.include_router(cliente_router, prefix="/cliente")
+app.include_router(cliente.router, prefix="/cliente")
 
 
 if __name__ == "__main__":
--- routes/prestador/prestador.py
+++ routes/prestador/prestador.py
@@ -7,42 +7,42 @@ router = APIRouter(prefix="/prestador", tags=["Prestador"])
 
 
 # Rota para página inicial do Prestador
-@router.get("/", response_class=HTMLResponse, name="home_prestador")
+@router.get("/")
 async def home_prestador(request: Request):
     return templates.TemplateResponse("prestador/home.html", {"request": request})
 
 # Rota para painel do Prestador
-@router.get("/", response_class=HTMLResponse, name="painel_prestador")
+@router.get("/painel")
 async def painel_prestador(request: Request):
     return templates.TemplateResponse("prestador/home/painel.html", {"request": request, "id_prestador": 1, "pagina_ativa": "painel"})
 
 # Rota para listar serviços do Prestador 
-@router.get("/servicos", response_class=HTMLResponse, name="listar_servicos")
+@router.get("/servicos")
 async def listar_servicos(request: Request, q: Optional[str] = None):
     return templates.TemplateResponse("prestador/servico/servicos_listar.html", {"request": request, "servicos": "servicos", "q": q, "id_prestador": 1, "pagina_ativa": "servicos"})
 
 # Rota para cadastrar novo serviço
-@router.get("/servicos/novo", response_class=HTMLResponse, name="form_novo_servico")
+@router.get("/servicos/novo")
 async def form_novo_servico(request: Request):
     return templates.TemplateResponse("prestador/servico/servico_form.html", {"request": request, "acao": "novo", "id_prestador": 1, "pagina_ativa": "servicos"})
 
 # Rota para editar serviço 
-@router.get("/servicos/{id_servico}/editar", response_class=HTMLResponse, name="form_editar_servico")
+@router.get("/servicos/editar/{id_servico}")
 async def form_editar_servico(request: Request, id_servico: int):
     return templates.TemplateResponse("prestador/servico/servico_form.html", {"request": request, "servico": "servico", "acao": "editar", "id_prestador": 1, "pagina_ativa": "servicos"})
 
 # Rota para remover serviço
-@router.get("/servicos/{id_servico}/remover/")
+@router.get("/servicos/remover/{id_servico}")
 async def remover_servico(request: Request):
     return templates.TemplateResponse("prestador/servico/remover.html", {"request": request, "servico": "servico", "acao": "remover"})
 
 # Rota para agenda do prestador
-@router.get("/agenda", response_class=HTMLResponse, name="agenda_prestador")
+@router.get("/agenda")
 async def agenda_prestador(request: Request):
     return templates.TemplateResponse("prestador/home/agenda.html", {"request": request, "id_prestador": 1, "pagina_ativa": "agenda"})
 
 # Rota para assinatura de plano do prestador
-@router.get("/assinatura", response_class=HTMLResponse, name="assinatura_prestador")
+@router.get("/assinatura")
 async def assinatura_prestador(request: Request):
     return templates.TemplateResponse("prestador/home/assinatura.html", {"request": request, "id_prestador": 1, "pagina_ativa": "assinatura"})
 
@@ -62,7 +62,7 @@ async def prestador_solicitacoes(request: Request):
     return templates.TemplateResponse("prestador/home/solicitacoes.html", {"request": request, "solicitacoes": "solicitacoes", "id_prestador": 1, "pagina_ativa": "solicitacoes"})
 
 # Rota para responder a solicitação do prestador
-@router.get("/solicitacoes/{id_solicitacao}/responder")
+@router.get("/solicitacoes/responder/{id_solicitacao}")
 async def responder_solicitacao(request: Request, id_solicitacao: int):
     return templates.TemplateResponse("prestador/home/responder_solicitacao.html", {"request": request, "solicitacao": "solicitacao", "id_prestador": 1, "pagina_ativa": "solicitacoes"})
 
